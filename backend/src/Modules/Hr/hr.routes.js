4de8a39250f598207aff15eddaa068d88b003225
me-ziad <ziadgithub@gmail.com>
2026-01-15 14:16:49 +0200

    last

--- backend/src/Modules/Hr/hr.routes.js
+++ backend/src/Modules/Hr/hr.routes.js
@@ -9,8 +9,8 @@ router.get('/profile', auth, getProfile); // عرض بيانات HR
 router.put('/profile', auth, upload.single('logo'), updateProfile); // تعديل بيانات HR
 router.post('/profile/images', auth, upload.array('images', 10), addCompanyImages); 
 // ✅ جديد
-router.put("/hr/profile/images/update",auth, updateCompanyImage);
-router.delete("/hr/profile/images/:imageId",auth, deleteCompanyImage);
+router.put("/profile/images/update",auth, updateCompanyImage);
+router.delete("/profile/images/:imageId",auth, deleteCompanyImage);
 
 
 module.exports = router; 
