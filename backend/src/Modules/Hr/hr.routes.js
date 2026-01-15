f76018fbd3a0344df69fd6dc5644ddd66047d017
me-ziad <ziadgithub@gmail.com>
2026-01-15 12:37:34 +0200

    add u

--- backend/src/Modules/Hr/hr.routes.js
+++ backend/src/Modules/Hr/hr.routes.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth.middleware');
 const upload = require('../../middleware/upload');
-const { createProfile, getProfile, updateProfile, addCompanyImages } = require('./hr.controller');
+const { createProfile, getProfile, updateProfile, addCompanyImages, updateCompanyImage, deleteCompanyImage } = require('./hr.controller');
 
 router.post('/profile', auth, createProfile); // إنشاء HR جديد
 router.get('/profile', auth, getProfile); // عرض بيانات HR
 router.put('/profile', auth, upload.single('logo'), updateProfile); // تعديل بيانات HR
 router.post('/profile/images', auth, upload.array('images', 10), addCompanyImages); 
 // ✅ جديد
-router.put("/hr/profile/images/update", hrController.updateCompanyImage);
-router.delete("/hr/profile/images/:imageId", hrController.deleteCompanyImage);
+router.put("/hr/profile/images/update", updateCompanyImage);
+router.delete("/hr/profile/images/:imageId", deleteCompanyImage);
 
 
 module.exports = router; 
